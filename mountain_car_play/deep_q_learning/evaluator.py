663226b594fd63bcdcf1cf0466ccc150b69436af
Aayesha <aayesha@surfer-172-29-47-229-surfnet.walter-group.info>
2026-03-26 16:42:20 +0100

    updated repo

---
+++ mountain_car_play/deep_q_learning/evaluator.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+import cv2
+import tensorflow as tf
+from keras.models import load_model
+
+env = gym.make("MountainCar-v0", render_mode="rgb_array")
+q_net = load_model("dqnet2.keras")
+
+def policy(state, explore=0.0):
+    action = tf.argmax(q_net(state)[0], output_type=tf.int32)
+    if tf.random.uniform(shape=(), maxval=1) <= explore:
+        action = tf.random.uniform(shape=(), minval=0, maxval=3, dtype=tf.int32)
+    return action
+
+
+for episode in range(5):
+    done = False
+    obs, _ = env.reset()
+    state = tf.convert_to_tensor([obs])
+    while not done:
+        frame = env.render()
+        cv2.imshow("Mountain Car", frame)
+        cv2.waitKey(100)
+        action = policy(state)
+        state, reward, done, _, _ = env.step(action.numpy())
+        state = tf.convert_to_tensor([state])
+env.close()
